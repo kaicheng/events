ed5c30b7ddd89ba501ccb61fff25e51d569a0bac
Kaicheng Zhang <kaichengz@gmail.com>
2014-04-15 19:14:02 -0500

    Added goport.events.EventEmitter class and its test files.
    goport.events.EventEmitter class is a simple port of node's
    events.EventEmitter clas.

---
+++ eventemitter.go
@@ -0,0 +1,81 @@
+package engineio
+
+import (
+    "container/list"
+    "reflect"
+    "sync"
+)
+
+type listenerType map[string]*list.List
+type eventHandler struct {
+    fn reflect.Value
+    args []reflect.Type
+}
+
+type EventEmitter struct {
+    sync.RWMutex
+    listeners listenerType
+}
+
+func NewEventEmitter() (ee *EventEmitter) {
+    ee = new(EventEmitter)
+    ee.listeners = make(listenerType)
+    return
+}
+
+func getEventHandler(fn interface{}) (handler *eventHandler) {
+    fnValue := reflect.ValueOf(fn)
+    fnType := reflect.TypeOf(fn)
+    if fnType.Kind() != reflect.Func {
+        return nil
+    }
+
+    handler = new(eventHandler)
+    handler.fn = fnValue
+    handler.args = make([]reflect.Type, fnType.NumIn())
+
+    for i := range handler.args {
+        handler.args[i] = fnType.In(i)
+    }
+
+    return handler
+}
+
+func (ee *EventEmitter) On (event string, listener interface{}) {
+    el := getEventHandler(listener)
+    if (el == nil) {
+        return
+    }
+    ee.Lock()
+    defer ee.Unlock()
+    ls, found := ee.listeners[event]
+    if !found {
+        ls = list.New()
+        ee.listeners[event] = ls
+    }
+    ls.PushBack(el)
+}
+
+func tryCall(el *eventHandler, args []interface{}) {
+    if len(args) == len(el.args) {
+        callArgs := make([]reflect.Value, len(args))
+        for i, arg := range args {
+            callArgs[i] = reflect.ValueOf(arg)
+            if !reflect.TypeOf(arg).AssignableTo(el.args[i]) {
+                return;
+            }
+        }
+        go el.fn.Call(callArgs)
+    }
+}
+
+func (ee *EventEmitter) Emit (event string, args ...interface{}) {
+    ee.RLock()
+    defer ee.RUnlock()
+    ls, found := ee.listeners[event]
+    if found {
+        for l := ls.Front(); l != nil; l = l.Next() {
+            tryCall(l.Value.(*eventHandler), args)
+        }
+    }
+}
---
+++ eventemitter_test.go
@@ -0,0 +1,87 @@
+package engineio
+
+import (
+    "testing"
+)
+
+func TestSimpleEvent(t *testing.T) {
+    msg := "simple message"
+    ee := NewEventEmitter()
+    ee.On("message", func (msg string) {
+        t.Log("on:", msg)
+    })
+    t.Log("emitting:", msg)
+    ee.Emit("message", msg)
+}
+
+func TestInterfaceFn(t *testing.T) {
+    msg := "simple message"
+    ee := NewEventEmitter()
+    ee.On("message", func (msg interface{}) {
+        t.Log("on:", msg)
+    })
+    t.Log("emitting:", msg)
+    ee.Emit("message", msg)
+}
+
+func TestMultiHandler(t *testing.T) {
+    msg := "simple message"
+    ee := NewEventEmitter()
+    ee.On("message", func (msg string) {
+        t.Log("on (string):", msg)
+    })
+    ee.On("message", func (msg interface{}) {
+        t.Log("on (interface):", msg)
+    })
+    t.Log("emitting:", msg)
+    ee.Emit("message", msg)
+}
+
+type position struct {
+    x, y float32
+}
+
+func TestHandlerByType(t *testing.T) {
+    pos := new(position)
+    pos.x = 4.5
+    pos.y = 5.6
+
+    ee := NewEventEmitter()
+    ee.On("message", func (msg string) {
+        t.Log("on (string):", msg)
+    })
+    ee.On("message", func (msg interface{}) {
+        t.Log("on (interface):", msg)
+    })
+    ee.On("message", func (pos position) {
+        t.Log("on (position):", pos.x, pos.y)
+    })
+
+    t.Log("emitting:", *pos)
+    ee.Emit("message", *pos)
+}
+
+func TestHandlerByNArgs(t *testing.T) {
+    pos := new(position)
+    pos.x = 4.5
+    pos.y = 5.6
+
+    what := "car"
+    ee := NewEventEmitter()
+    ee.On("message", func (msg string) {
+        t.Log("on (string):", msg)
+    })
+    ee.On("message", func (msg string, pos position) {
+        t.Log("on (string, position):", msg, pos)
+    })
+    ee.On("message", func (msg string, pos position) string {
+        t.Log("on (string, position) (string):", msg, pos)
+        return msg
+    })
+    ee.On("message", func (msg string, pos position, ran interface{}) {
+        t.Log("on (string, position interface{}):", msg, pos, ran)
+    })
+
+    t.Log("emitting:", what, *pos)
+    ee.Emit("message", what, *pos)
+}
